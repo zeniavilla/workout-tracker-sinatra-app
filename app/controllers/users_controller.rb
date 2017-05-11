c3252131d0f41c0aded477372b7bae82cd0333bc
Zenia Villa <zeniavilla@hotmail.com>
2017-05-11 14:46:02 -0700

    add signup action to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,22 @@
 class UsersController < ApplicationController
 
+    get '/users/signup' do
+        erb :'/users/signup'
+    end
+
+    post '/users/signup' do
+        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+        @user.save
+        if @user.save
+            redirect "/workouts/index"
+        else
+            redirect back
+        end
+    end
+    
+    get '/users/login' do
+        erb :'/users/login'
+    end
+
+    
 end
