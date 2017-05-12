75db1e3eea31b25921f26c4f26cc30ceb0bf986d
Zenia Villa <zeniavilla@hotmail.com>
2017-05-11 17:32:13 -0700

    Create route actions for login and signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
+require 'rack-flash'
+
 class UsersController < ApplicationController
+    use Rack::Flash
 
     get '/users/signup' do
         erb :'/users/signup'
@@ -8,8 +11,11 @@ class UsersController < ApplicationController
         @user = User.new(username: params[:username], email: params[:email], password: params[:password])
         @user.save
         if @user.save
+            session[:id] = @user.id
+            flash[:success_login] = "Successfully created account."
             redirect "/workouts/index"
         else
+            flash[:error_signup] = "Please enter a username, email, and password."
             redirect back
         end
     end
@@ -18,5 +24,15 @@ class UsersController < ApplicationController
         erb :'/users/login'
     end
 
+    post '/users/login' do
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
+            session[:id] = @user.id
+            redirect "/workouts/index"
+        else
+            flash[:error_login] = "Please enter a valid username and password."
+            redirect back
+        end
+    end
     
 end
